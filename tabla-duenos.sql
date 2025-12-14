8dc4f878d505001eb4e542502e462d6f536f353d
luisinavinuela <luisina.v@hotmail.com>
2025-12-14 20:40:01 -0300

    ejercicio-1y2

---
+++ tabla-duenos.sql
@@ -0,0 +1,8 @@
+--DUEÑOS
+CREATE TABLE duenos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    direccion VARCHAR(100)
+);
