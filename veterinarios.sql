3929aa8ce080e559b600cd93ecfbbc83a6096b89
luisinavinuela <luisina.v@hotmail.com>
2025-12-14 20:56:06 -0300

    ejercicio-4

---
+++ veterinarios.sql
@@ -0,0 +1,8 @@
+--VETERINARIOS
+CREATE TABLE veterinarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    matricula VARCHAR(20) NOT NULL UNIQUE,
+    especialidad VARCHAR(50) NOT NULL
+);
