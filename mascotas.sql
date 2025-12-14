3eb02a6951a827a727dd554a34ef8e787d90d7df
luisinavinuela <luisina.v@hotmail.com>
2025-12-14 20:50:14 -0300

    ejercicio-3

---
+++ mascotas.sql
@@ -0,0 +1,9 @@
+--MASCOTAS
+CREATE TABLE mascotas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    especie VARCHAR(30) NOT NULL,
+    fecha_nacimiento DATE,
+    id_dueno INT,
+    FOREIGN KEY (id_dueno) REFERENCES duenos(id)
+);
