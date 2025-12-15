4fcda9bbbcbfedb78766d78d6923abaeb69cbad7
luisinavinuela <luisina.v@hotmail.com>
2025-12-15 16:28:24 -0300

    6-insertar-registros-actualización

--- 6-insertar-registros.sql
+++ 6-insertar-registros.sql
@@ -17,3 +17,10 @@ INSERT INTO veterinarios (nombre, apellido, matricula, especialidad)
 VALUES
 ("Lautaro", "Rastelli", "11223344", "rayos"),
 ("Octavio", "Moreno", "55667788", "clinica");
+
+--INSERTAR HISTORIAL CLINICO
+INSERT INTO historial_clinico (id_mascota, id_veterinario, descripcion)
+VALUES
+(1, 2, "Un gato doméstico de pelaje blanco ingresó a la veterinaria acompañado por su tutor, presentando una lesión cutánea visible. El animal se mostró alerta pero incómodo al manipular la zona afectada, motivo principal de la consulta clínica."),
+(2, 1, "Un perro de tamaño mediano ingresó a la veterinaria acompañado por su tutor para la realización de una placa radiográfica. El procedimiento fue solicitado con el fin de evaluar una posible lesión interna y obtener un diagnóstico más preciso."),
+(3, 2, "Un canario de plumaje amarillo ingresó a la veterinaria acompañado por su tutor debido a una lesión en el ala derecha. El ave presentaba dificultad para moverla y signos de dolor, motivo principal de la consulta clínica.");
